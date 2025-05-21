0a20cf18c358bfffd560d10eeec5fcd2f6b2bda4
SvetaVarghese <svetlanavarghese@gmail.com>
2025-05-21 19:56:57 +0300

    change modal.js, fix graph-modal

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -31,7 +31,7 @@
 
 // Реализация модального окна
 import GraphModal from 'graph-modal';
-const modal = new GraphModal();
+export const modal = new GraphModal();
 
 // Реализация табов
 import GraphTabs from 'graph-tabs';
