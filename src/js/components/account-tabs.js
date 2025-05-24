7b605b36770507bcf608d2a77bbf8d02380ccf5e
SvetaVarghese <svetlanavarghese@gmail.com>
2025-05-24 14:17:50 +0300

    mobile for account-orders, account-purchases, order-info

--- src/js/components/account-tabs.js
+++ src/js/components/account-tabs.js
@@ -6,49 +6,44 @@ document.addEventListener('DOMContentLoaded', function (e) {
     const tabs = new GraphTabs('account-tabs', {
       isChanged: () => {
 
-      // Скрывать меню табов только на мобильных
+        // Функция скрытия и показа h1
+        const title = document.querySelector('.account-content__title');
+        function toggleAccountTitle(hide) {
+          if (title) {
+            title.style.display = hide ? 'none' : '';
+          }
+        }
+
+        // Скрывать меню табов только на мобильных
         if (isMobile()) {
           const tabNav = document.querySelector('.tabs__nav');
           if (tabNav) tabNav.style.display = 'none';
 
           const activePanel = tabs.container.querySelector('.tabs__panel--active');
           if (activePanel) activePanel.classList.add('force-open');
+
+          // Деактивируем стандартное поведение GraphTabs
+          tabs.switchTabs = () => { };
+
+          // Вручную управляем панелями
+          document.querySelectorAll('.tabs__nav [data-tab]').forEach(button => {
+            button.addEventListener('click', () => {
+              const tabId = button.dataset.tab;
+              document.querySelectorAll('.tabs__panel').forEach(panel => {
+                panel.style.display = panel.dataset.tabContent === tabId ? 'block' : 'none';
+              });
+            });
+          });
+
+          // 3. Показываем навигацию
+          if (tabNav) tabNav.style.display = 'block';
+
+          // Скрываем h1 на мобильных
+          toggleAccountTitle(true);
+        } else {
+          toggleAccountTitle(false);
         }
       }
     });
   }
-
-  if (isMobile()) {
-    // Деактивируем стандартное поведение GraphTabs
-    tabs.switchTabs = () => {};
-
-    // Вручную управляем панелями
-    document.querySelectorAll('.tabs__nav [data-tab]').forEach(button => {
-      button.addEventListener('click', () => {
-        const tabId = button.dataset.tab;
-        document.querySelectorAll('.tabs__panel').forEach(panel => {
-          panel.style.display = panel.dataset.tabContent === tabId ? 'block' : 'none';
-        });
-      });
-    });
-
-      // 3. Показываем навигацию
-    const tabNav = document.querySelector('.tabs__nav');
-    if (tabNav) tabNav.style.display = 'block';
-  }
 })
-
-
-
-
-    // if (isMobile()) {
-    //   setTimeout(() => {
-    //     // скрываем панели на мобильных
-    //     document.querySelectorAll('.tabs__panel').forEach(panel => {
-    //       panel.classList.remove('tabs__panel--active');
-    //     });
-    //     const tabNav = document.querySelector('.tabs__nav');
-    //     if (tabNav) tabNav.style.display = 'block';
-    //   }, 0);
-    // }
-
